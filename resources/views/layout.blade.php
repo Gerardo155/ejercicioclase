a50fea6677ecf3ccf00254015cb7e42f10dbe000
Gerardo155 <alimah@miumg.edu.gt>
2023-05-24 23:09:39 -0600

    agregando SweetAlert

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>App Name - @yield('title')</title>
+</head>
+<body>
+@section('sidebar')
+    Pagina principal
+    <br>
+    <br>
+
+@show
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
+@section('js')
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <script>
+
+        swal("¡Ventana emergente!");
+
+
+    </script>
+@endsection
